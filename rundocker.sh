0b9359fff94b992736c3f3bc6574db182c508449
Thomas Fragner <fragner@bitter.at>
2018-09-23 14:20:50 +0200

    update rundocker to compile-pdf.sh

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $1
-docker run --rm -v $PWD:/usr/share/blog meroff/hugo-with-pandoc ./bin/compile-all.sh $1
+docker run --rm -v $PWD:/usr/share/blog meroff/hugo-with-pandoc ./bin/compile-pdf.sh $1
