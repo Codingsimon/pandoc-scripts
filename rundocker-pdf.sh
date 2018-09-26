fec70a1399f11b9cb01a7c2e767c6cf14ce7a3e3
Thomas Fragner <fragner@bitter.at>
2018-09-26 21:09:16 +0200

    ed

--- rundocker-pdf.sh
+++ rundocker-pdf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v $PWD:/usr/share/blog meroff/hugo-with-pandoc ./bin/compile-pdf.sh $1
+docker run --rm -v $PWD:/usr/share/blog meroff/hugo-with-pandoc:latest ./bin/compile-pdf.sh $1
