80d92a2b7f4708b30d8b3e50f9a619fc8f4e716c
Thomas Fragner <fragner@bitter.at>
2018-10-02 12:15:17 +0200

    added option for hugo export of PDFs

--- rundocker-all.sh
+++ rundocker-all.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-docker run --rm -v $PWD:/usr/share/blog meroff/hugo-with-pandoc ./bin/compile-pdf.sh $1
-docker run --rm -v $PWD:/usr/share/blog meroff/hugo-with-pandoc ./bin/compile-html.sh $1
+
+POSITIONAL=()
+
+while [[ $# -gt 0 ]] ; do
+    key="$1"
+
+    case $key in
+        --hugo)
+            EXPORT_DIR="./"
+            shift
+            ;;
+        *)    # unknown option
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+docker run --rm -v $PWD:/usr/share/blog meroff/hugo-with-pandoc ./bin/compile-pdf.sh ${EXPORT_DIR}
+docker run --rm -v $PWD:/usr/share/blog meroff/hugo-with-pandoc ./bin/compile-html.sh
