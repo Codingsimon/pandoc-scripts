c0fc457992069ad249e413854a69a380424d63b1
Thomas Fragner <fragner@bitter.at>
2018-12-26 12:52:05 +0100

    allow blanks in paths

--- make-files.sh
+++ make-files.sh
@@ -97,8 +97,8 @@ if [[ ${BOOK} = true ]] ; then
                 print_empty_lines ${FILENAME_TEMP}
             fi
             # add the source file content without frontmatter
-            LINES=$(sed ${SED_YAML_HEADER} ${p} | wc -l)
-            awk "NR > $LINES" < $p | sed 's@\(!\[.*\]\)(\(.*\))\(.*\)@\1('"$DIR"'\/\2)\3@g' >> $FILENAME_TEMP
+            LINES=$(sed ${SED_YAML_HEADER} "${p}" | wc -l)
+            awk "NR > $LINES" < "$p" | sed 's@\(!\[.*\]\)(\(.*\))\(.*\)@\1('"$DIR"'\/\2)\3@g' >> $FILENAME_TEMP
             print_empty_lines ${FILENAME_TEMP}
         fi
     done < $FILENAME_TEMP.index  
