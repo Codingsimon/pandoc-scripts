cf02eaf6f4d71c955ab91f851216204286961db9
Peter <61778415+Pawedla@users.noreply.github.com>
2020-12-20 16:08:47 +0100

    Update compile-pdf.sh

--- compile-pdf.sh
+++ compile-pdf.sh
@@ -23,7 +23,7 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 # Einrichten einer Python Umgebung innerhalb des containers
-apt-get -qq update && DEBIAN_FRONTEND=noninteractive && apt-get -qq install -y python3-venv python3-wheel
+apt-get -qq update && DEBIAN_FRONTEND=noninteractive && apt-get -qq install -y python3-venv python3-dev python3-wheel
 python3 -m venv venv
 
 # Aktivieren der Python Umgebung
