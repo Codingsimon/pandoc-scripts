ee36490d96dc618740126aabeac9ee56ccf01bd4
Peter <61778415+Pawedla@users.noreply.github.com>
2020-12-20 19:48:42 +0100

    Update run-hugo-server.sh

--- run-hugo-server.sh
+++ run-hugo-server.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-cd /usr/share/blog && python3.6 -m venv venv && source venv/bin/activate && pip install -r ./.pandoc/requirements.txt && cp -purv .pandoc ~/ && hugo server -b ${HUGO_BASE_URL} --bind=0.0.0.0
+
