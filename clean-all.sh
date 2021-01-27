49031db55d0ddf65119348504dace4445a78cc19
Peter <61778415+Pawedla@users.noreply.github.com>
2021-01-27 13:22:13 +0100

    Delete clean-all.sh

--- clean-all.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-find -type f -name "*.sh" -exec chmod 755 {} \;
-find . -name "tex2pdf*" -type d -prune -exec rm -rf '{}' '+'
-find -type f -name "TEMP*" -exec rm {} \;
-find -type f -name "start.sh" -exec rm {} \;
