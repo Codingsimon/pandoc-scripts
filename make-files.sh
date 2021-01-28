d964629fa8a6b9dce5d05615d5132867a72e7187
Peter <61778415+Pawedla@users.noreply.github.com>
2021-01-28 22:02:22 +0100

    Update make-files.sh

--- make-files.sh
+++ make-files.sh
@@ -80,9 +80,16 @@ if [[ ${BOOK} = true ]] ; then
             if [[ ! $(basename "${p}") = "_"* ]] ; then
 	    echo testStart
 	    cat ${FILENAME_TEMP}
-	     echo testEnd
                 echo "#" `sed ${SED_YAML_HEADER} "${p}" | grep "title:" | sed 's/^[^:]*:[[:space:]]*//'` >> $FILENAME_TEMP
+		echo test1
+	    cat ${FILENAME_TEMP}
                 print_empty_lines ${FILENAME_TEMP}
+		
+		echo test2
+	    cat ${FILENAME_TEMP}
+		
+			     echo testEnd
+
             fi
             # add the source file content without frontmatter
             LINES=$(sed ${SED_YAML_HEADER} "${p}" | wc -l)
